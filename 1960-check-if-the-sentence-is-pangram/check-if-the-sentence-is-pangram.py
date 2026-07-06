19b3cfe4a47565f5a633f231c6e95bf93f3eed34
Selina Bennet A <selinabennet2007@gmail.com>
2026-07-06 22:04:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (49.21%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfPangram(self, sentence: str) -> bool:
+        for i in "abcdefghijklmnopqrstuvwxyz":
+            if i not in sentence:
+                return False
+        return True
+
+        
